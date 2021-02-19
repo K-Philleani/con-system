5e779728db6ef7f889fee6fdac51f611db357646
K-Philleani <964202623@qq.com>
2021-02-19 22:38:00 +0800

    repository 删除，更新，查询单条数据

--- datamodels/product.go
+++ datamodels/product.go
@@ -1,9 +1,9 @@
 package datamodels
 
 type Product struct {
-	ID           int64  `json:"id" sql:"id" cons:"id"`
-	ProductName  string `json:"productName" sql:"productName" cons:"productName"`
-	ProductNum   int64  `json:"productNum" sql:"productNum" cons:"productNum"`
-	ProductImage string `json:"productImage" sql:"productImage" cons:"productImage"`
-	ProductUrl   string `json:"productUrl" sql:"productUrl" cons:"productUrl"`
+	ID           int64  `json:"id" sql:"ID" cons:"ID"`
+	ProductName  string `json:"ProductName" sql:"productName" cons:"ProductName"`
+	ProductNum   int64  `json:"ProductNum" sql:"productNum" cons:"ProductNum"`
+	ProductImage string `json:"ProductImage" sql:"productImage" cons:"ProductImage"`
+	ProductUrl   string `json:"ProductUrl" sql:"productUrl" cons:"ProductUrl"`
 }
--- repositories/product_repository.go
+++ repositories/product_repository.go
@@ -4,6 +4,7 @@ import (
 	"con-system/common"
 	"con-system/datamodels"
 	"database/sql"
+	"strconv"
 )
 
 // 第一步：开发对应的接口
@@ -58,4 +59,63 @@ func (p *ProductManager) Insert(product *datamodels.Product) (productId int64, e
 		return 0, err
 	}
 	return result.LastInsertId()
+}
+
+// 删除
+func (p *ProductManager) Delete(productID int64) bool{
+	if err := p.Conn(); err != nil {
+		return false
+	}
+	sql := "deletd from product where ID=?"
+	stmt, err := p.mysqlConn.Prepare(sql)
+	if err != nil {
+		return false
+	}
+	_, err = stmt.Exec(productID)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// 更新
+func (p *ProductManager) Update(product *datamodels.Product) error{
+	if err := p.Conn(); err != nil {
+		return err
+	}
+	sql := "update product set productName=?, productNum=?, productImage=?, productUrl=? where ID=" + strconv.FormatInt(product.ID, 10)
+	stmt, err := p.mysqlConn.Prepare(sql)
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(product.ProductName, product.ProductNum, product.ProductImage, product.ProductUrl)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//
+func (p ProductManager) Select(productID int64) (productResult *datamodels.Product, err error){
+	if err = p.Conn(); err != nil {
+		return &datamodels.Product{}, err
+	}
+	sql := "select * from" + p.table + "where ID =" + strconv.FormatInt(productID, 10)
+	row, err := p.mysqlConn.Query(sql)
+	if err != nil {
+		return &datamodels.Product{}, err
+	}
+	result := common.GetResultRow(row)
+	if len(result) == 0 {
+		return &datamodels.Product{}, nil
+	}
+	productResult = &datamodels.Product{}
+	common.DataToStructByTagSql(result,productResult)
+	return
+
+}
+
+//
+func (p *ProductManager) SelectAll() ([]*datamodels.Product, error) {
+	return nil, nil
 }
