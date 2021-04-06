8f47c28e27c39af719e966c73dafe8030c9af350
K-Philleani <964202623@qq.com>
2021-04-06 23:57:33 +0800

    整理代码

--- main.go
+++ main.go
@@ -6,16 +6,25 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+var db *sql.DB
+
+func initMysql() (err error){
 	dsn := "root:123456@tcp(124.70.71.78:3306)/db1"
-	db, err := sql.Open("mysql", dsn)
+	db, err = sql.Open("mysql", dsn)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	defer db.Close()
 	err = db.Ping()
 	if err != nil {
-		panic(err)
+		return err
 	}
+	return
+}
+
+func main() {
+	if err := initMysql(); err != nil {
+		fmt.Println("连接数据库失败")
+	}
+	defer db.Close()
 	fmt.Println("数据库连接成功")
 }
