4ca600820e4f0eb654a8332e3630965c88352827
K-Philleani <964202623@qq.com>
2021-04-07 23:03:28 +0800

    mysql 单行查找

--- main.go
+++ main.go
@@ -8,8 +8,14 @@ import (
 
 var db *sql.DB
 
+type User struct {
+	id int
+	name string
+	age int
+}
+
 func initMysql() (err error){
-	dsn := "root:123456@tcp(124.70.71.78:3306)/db1"
+	dsn := "root:123456@tcp(124.70.71.78:3306)/cons"
 	db, err = sql.Open("mysql", dsn)
 	if err != nil {
 		return err
@@ -21,10 +27,22 @@ func initMysql() (err error){
 	return
 }
 
+func queryRowDemo() {
+	sqlStr := "select id, name, age from user where id = ?"
+	var u User
+	err := db.QueryRow(sqlStr, 1).Scan(&u.id, &u.name, &u.age)
+	if err != nil {
+		fmt.Printf("scan failed, err: %v\n", err)
+		return
+	}
+	fmt.Printf("id: %d name: %s age:%d\n", u.id, u.name, u.age)
+}
+
 func main() {
 	if err := initMysql(); err != nil {
 		fmt.Println("连接数据库失败")
 	}
 	defer db.Close()
 	fmt.Println("数据库连接成功")
+	queryRowDemo()
 }
