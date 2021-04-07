67a7c84cd03f35b2db8a68c4f8171daf9ac76fe9
K-Philleani <964202623@qq.com>
2021-04-07 23:25:11 +0800

    mysql 多行查找&&插入数据

--- main.go
+++ main.go
@@ -38,6 +38,45 @@ func queryRowDemo() {
 	fmt.Printf("id: %d name: %s age:%d\n", u.id, u.name, u.age)
 }
 
+func queryMultiRowDemo() {
+	sqlStr := "select id, name, age from user where id > ?"
+	rows, err := db.Query(sqlStr, 0)
+	if err != nil {
+		fmt.Printf("query failed, err: %v\n", err)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var u User
+		err := rows.Scan(&u.id, &u.name, &u.age)
+		if err != nil {
+			fmt.Printf("scan failed, err: %v\n", err)
+			return
+		}
+		fmt.Printf("id: %d name:%s age:%d\n", u.id, u.name, u.age)
+	}
+}
+
+func insertRowDemo() {
+	sqlStr := "insert into user (name, age) values(?, ?)"
+	ret, err := db.Exec(sqlStr, "宋元策", 25)
+	if err != nil {
+		fmt.Printf("insert failed, err: %v\n", err)
+		return
+	}
+	theId, err := ret.LastInsertId()
+	if err != nil {
+		fmt.Printf("get lastinsertid failed, err:%v\n", err)
+		return
+	}
+	fmt.Printf("insert success, the id is: %d\n", theId)
+}
+
+func updateDemo() {
+
+}
+
 func main() {
 	if err := initMysql(); err != nil {
 		fmt.Println("连接数据库失败")
@@ -45,4 +84,8 @@ func main() {
 	defer db.Close()
 	fmt.Println("数据库连接成功")
 	queryRowDemo()
+	fmt.Println("==========================")
+	queryMultiRowDemo()
+	fmt.Println("==========================")
+	insertRowDemo()
 }
